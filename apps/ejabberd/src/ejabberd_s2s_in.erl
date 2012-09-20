60b9e71d767dd779c63c0fe9e6ef04aefa79303b
Jan Daniel Bothma <jan.bothma@erlang-solutions.com>
2012-09-20 11:44:17 +0200

    Check domain of receiving server in its cert for s2s with s2s_use_starttls _required.

--- apps/ejabberd/src/ejabberd_s2s_in.erl
+++ apps/ejabberd/src/ejabberd_s2s_in.erl
@@ -32,7 +32,6 @@
 %% External exports
 -export([start/2,
 	 start_link/2,
-	 match_domain/2,
 	 socket_type/0]).
 
 %% gen_fsm callbacks
@@ -49,8 +48,6 @@
 -include("ejabberd.hrl").
 -include("jlib.hrl").
 -include_lib("public_key/include/public_key.hrl").
--define(PKIXEXPLICIT, 'OTP-PUB-KEY').
--define(PKIXIMPLICIT, 'OTP-PUB-KEY').
 -include("XmppAddr.hrl").
 
 -define(DICT, dict).
@@ -292,9 +289,8 @@ wait_for_feature_request({xmlstreamelement, El}, StateData) ->
 		    AuthRes =
 			case (StateData#state.sockmod):get_peer_certificate(
 			       StateData#state.socket) of
-			    {ok, CertBin} ->
-                                Cert = public_key:pkix_decode_cert(CertBin, plain),
-                                auth_domain_matches_cert(AuthDomain, Cert);
+			    {ok, Cert} ->
+                                ejabberd_tls:domain_matches_cert(AuthDomain, Cert);
                             {error, no_peercert} ->
                                 ?WARNING_MSG("no peer cert from ~p in SASL EXT",
                                           [AuthDomain]),
@@ -650,135 +646,3 @@ is_key_packet({xmlelement, Name, Attrs, Els}) when Name == <<"db:verify">> ->
 is_key_packet(_) ->
     false.
 
-auth_domain_matches_cert(error, _)->
-    false;
-auth_domain_matches_cert(AuthDomain, Cert) ->
-    case idna:domain_utf8_to_ascii(AuthDomain) of
-        false ->
-            false;
-        PCAuthDomain ->
-            CertDomains = get_cert_domains(Cert),
-            ?DEBUG("AuthDomain: ~p  CertDomains: ~p", [AuthDomain,CertDomains]),
-            lists:any(fun(D) ->
-                              match_domain(PCAuthDomain, D)
-                      end,
-                      CertDomains)
-    end.
-
-get_cert_domains(Cert = #'Certificate'{}) ->
-    {rdnSequence, Subject} =
-	(Cert#'Certificate'.tbsCertificate)#'TBSCertificate'.subject,
-    Extensions =
-	(Cert#'Certificate'.tbsCertificate)#'TBSCertificate'.extensions,
-    lists:flatmap(
-      fun(#'AttributeTypeAndValue'{type = ?'id-at-commonName',
-				   value = Val}) ->
-	      case ?PKIXEXPLICIT:decode('X520CommonName', Val) of
-		  {ok, {_, D1}} ->
-		      D = if
-			      is_list(D1) -> list_to_binary(D1);
-			      is_binary(D1) -> D1;
-			      true -> error
-			  end,
-		      if
-			  D /= error ->
-			      case jlib:binary_to_jid(D) of
-				  #jid{luser = <<"">>,
-				       lserver = LD,
-				       lresource = <<"">>} ->
-				      [LD];
-				  _ ->
-				      []
-			      end;
-			  true ->
-			      []
-		      end;
-		  _ ->
-		      []
-	      end;
-	 (_) ->
-	      []
-      end, lists:flatten(Subject)) ++
-	lists:flatmap(
-	  fun(#'Extension'{extnID = ?'id-ce-subjectAltName',
-			   extnValue = Val}) ->
-		  BVal = if
-			     is_list(Val) -> list_to_binary(Val);
-			     is_binary(Val) -> Val;
-			     true -> Val
-			 end,
-		  case ?PKIXIMPLICIT:decode('SubjectAltName', BVal) of
-		      {ok, SANs} ->
-			  lists:flatmap(
-			    fun({otherName,
-				 #'AnotherName'{'type-id' = ?'id-on-xmppAddr',
-						value = XmppAddr
-					       }}) ->
-				    case 'XmppAddr':decode(
-					   'XmppAddr', XmppAddr) of
-					{ok, D} when is_binary(D) ->
-					    case jlib:binary_to_jid(D) of
-						#jid{luser = <<"">>,
-						     lserver = LD,
-						     lresource = <<"">>} ->
-						    case idna:domain_utf8_to_ascii(LD) of
-							false ->
-							    [];
-							PCLD ->
-							    [PCLD]
-						    end;
-						_ ->
-						    []
-					    end;
-					_ ->
-					    []
-				    end;
-			       ({dNSName, D}) when is_list(D) ->
-				    case jlib:binary_to_jid(list_to_binary(D)) of
-					#jid{luser = <<"">>,
-					     lserver = LD,
-					     lresource = <<"">>} ->
-					    [LD];
-					_ ->
-					    []
-				    end;
-			       (_) ->
-				    []
-			    end, SANs);
-		      _ ->
-			  []
-		  end;
-	     (_) ->
-		  []
-	  end, Extensions).
-
-match_domain(Domain, Domain) ->
-    true;
-match_domain(Domain, Pattern) ->
-    DLabels = binary:split(Domain, <<".">>, [global]),
-    PLabels = binary:split(Pattern, <<".">>, [global]),
-    match_labels(DLabels, PLabels).
-
-match_labels([], []) ->
-    true;
-match_labels([], [_ | _]) ->
-    false;
-match_labels([_ | _], []) ->
-    false;
-match_labels([DL | DLabels], [PL | PLabels]) ->
-    PLlist = binary_to_list(PL),
-    case lists:all(fun(C) -> (($a =< C) andalso (C =< $z))
-				 orelse (($0 =< C) andalso (C =< $9))
-				 orelse (C == $-) orelse (C == $*)
-		   end, PLlist) of
-	true ->
-	    Regexp = xmerl_regexp:sh_to_awk(PLlist),
-	    case re:run(binary_to_list(DL), Regexp, [{capture, none}]) of
-		match ->
-		    match_labels(DLabels, PLabels);
-		nomatch ->
-		    false
-	    end;
-	false ->
-	    false
-    end.
